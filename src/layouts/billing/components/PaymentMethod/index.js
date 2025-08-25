f4ca6c7b13880d366a407e515c233e30236196e6
sandeepwns <sandeeppwns@gmail.com>
2025-08-25 10:59:00 +0530

    code push first time

---
+++ src/layouts/billing/components/PaymentMethod/index.js
@@ -0,0 +1,115 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import Icon from "@mui/material/Icon";
+import Tooltip from "@mui/material/Tooltip";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDButton from "components/MDButton";
+
+// Images
+import masterCardLogo from "assets/images/logos/mastercard.png";
+import visaLogo from "assets/images/logos/visa.png";
+
+// Material Dashboard 2 React context
+import { useMaterialUIController } from "context";
+
+function PaymentMethod() {
+  const [controller] = useMaterialUIController();
+  const { darkMode } = controller;
+
+  return (
+    <Card id="delete-account">
+      <MDBox pt={2} px={2} display="flex" justifyContent="space-between" alignItems="center">
+        <MDTypography variant="h6" fontWeight="medium">
+          Api Key
+        </MDTypography>
+      </MDBox>
+      <MDBox p={2}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <MDTypography variant="subtitle2" color="textSecondary" gutterBottom>
+              Gate.io Public Key
+            </MDTypography>
+            <MDBox
+              borderRadius="lg"
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              p={3}
+              sx={{
+                border: ({ borders: { borderWidth, borderColor } }) =>
+                  `${borderWidth[1]} solid ${borderColor}`,
+              }}
+            >
+              <MDTypography variant="h6" fontWeight="medium">
+                ****&nbsp;&nbsp;****&nbsp;&nbsp;****&nbsp;&nbsp;SJHSDU
+              </MDTypography>
+              <MDBox ml="auto" lineHeight={0} color={darkMode ? "white" : "dark"}>
+                <Tooltip title="Edit Api" placement="top">
+                  <Icon sx={{ cursor: "pointer" }} fontSize="small">
+                    edit
+                  </Icon>
+                </Tooltip>
+              </MDBox>
+            </MDBox>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <MDTypography variant="subtitle2" color="textSecondary" gutterBottom>
+              Gate.io Secret Key
+            </MDTypography>
+            <MDBox
+              borderRadius="lg"
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              p={3}
+              sx={{
+                border: ({ borders: { borderWidth, borderColor } }) =>
+                  `${borderWidth[1]} solid ${borderColor}`,
+              }}
+            >
+              <MDTypography variant="h6" fontWeight="medium">
+                ****&nbsp;&nbsp;****&nbsp;&nbsp;****&nbsp;&nbsp;HDHEA
+              </MDTypography>
+              <MDBox ml="auto" lineHeight={0} color={darkMode ? "white" : "dark"}>
+                <Tooltip title="Edit Api" placement="top">
+                  <Icon sx={{ cursor: "pointer" }} fontSize="small">
+                    edit
+                  </Icon>
+                </Tooltip>
+              </MDBox>
+            </MDBox>
+          </Grid>
+
+          {/* Save Button row ke last me */}
+          <Grid item xs={12} display="flex" justifyContent="flex-end">
+            <MDButton variant="contained" color="info">
+              Save
+            </MDButton>
+          </Grid>
+        </Grid>
+      </MDBox>
+    </Card>
+  );
+}
+
+export default PaymentMethod;
---
+++ src/layouts/dashboard/index.js
@@ -0,0 +1,66 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+
+// Data
+import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
+import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
+
+// Dashboard components
+import Projects from "layouts/dashboard/components/Projects";
+import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
+
+function Dashboard() {
+  const { sales, tasks } = reportsLineChartData;
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox py={3}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <div style={{ borderRadius: "10px", overflow: "hidden" }}>
+              <iframe
+                src="https://s.tradingview.com/widgetembed/?symbol=BINANCE:BTCUSDT&interval=30&hidesidetoolbar=1&theme=light"
+                width="100%"
+                height="500"
+                frameBorder="0"
+                // allowTransparency="true"
+                scrolling="no"
+                title="TradingView Chart"
+              ></iframe>
+            </div>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Dashboard;
