d36556e9de3b5efe089db10a7dce7cac200e18e5
sandeepwns <wns>
2025-08-29 13:41:20 +0530

    auto-button change

--- src/examples/Navbars/DashboardNavbar/index.js
+++ src/examples/Navbars/DashboardNavbar/index.js
@@ -303,7 +303,7 @@ function DashboardNavbar({ absolute, light, isMini }) {
                     },
                   }}
                 >
-                  Auto Trading
+                  {t("autoTrading")}
                 </FormLabel>
 
                 {/* Switch with Yes / No */}
@@ -312,7 +312,7 @@ function DashboardNavbar({ absolute, light, isMini }) {
                     variant="body2"
                     sx={{ fontSize: "12px", fontWeight: 500, color: "#9ea2b5" }}
                   >
-                    No
+                    {t("off")}
                   </Typography>
 
                   <Switch
@@ -334,7 +334,7 @@ function DashboardNavbar({ absolute, light, isMini }) {
                     variant="body2"
                     sx={{ fontSize: "12px", fontWeight: 500, color: "#9ea2b5" }}
                   >
-                    Yes
+                    {t("on")}
                   </Typography>
                 </Box>
               </FormControl>
